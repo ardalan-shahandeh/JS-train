9634c82e35bad51a0eea3aa90ed4c56cd96001cb
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-12-04 23:24:46 +0330

    Scroll Andicator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const toTop = document.querySelector(".to-top");
 
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 300) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
