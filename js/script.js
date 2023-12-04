cd07d258cb2ee49f237d693cf940d99b2a2bb89d
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-12-04 23:17:50 +0330

    fix: Main Branch

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-const container = document.querySelector("#container");
 
-window.onmousemove = function (e) {
-  let x = e.clientX;
-  let y = e.clientY;
-
-  container.style.backgroundPositionX = x / 5 + "px";
-  container.style.backgroundPositionY = y / 5 + "px";
-};
