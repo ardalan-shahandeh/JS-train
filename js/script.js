35ded2109ded294c75b106acfdea42e6b3772977
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-12-05 23:19:39 +0330

    Clean Code For Main Branch

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 300) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-});
